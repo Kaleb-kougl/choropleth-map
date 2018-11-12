4b441de1d876553a6fadfa845b7107b146ed0e7b
Kaleb Kougl <kalebkougl@gmail.com>
2018-11-12 09:03:58 -0600

    adding title, legend, adding margin, fixing tooltip bug

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ HTTP.onreadystatechange = function(){
   HTTP2.send();
     if (this.readyState == 4 && this.status == 200) {
         datasetJSON = JSON.parse(HTTP.responseText);
-        // console.log(datasetJSON);
         secondAJAX();
     } else {
         console.log("something went wrong");
@@ -24,11 +23,8 @@ HTTP.onreadystatechange = function(){
 
 function secondAJAX() {
   HTTP2.onreadystatechange = function(){
-    // console.log(2)
     if (this.readyState == 4 && this.status == 200) {
       educationDataset = JSON.parse(HTTP2.responseText);
-      // console.log('worked!');
-      // console.log(educationDataset);
       d3Commands();
     } else {
       console.log('Something went wrong with 2nd request');
@@ -38,8 +34,8 @@ function secondAJAX() {
 
 function d3Commands() {
   
-  let margin = { top: 0, left: 0, right: 0, bottom: 0, },
-  height = 600 - margin.top - margin.bottom,
+  let margin = { top: 30, left: 0, right: 0, bottom: 0, },
+  height = 700 - margin.top - margin.bottom,
   width = 950 - margin.left - margin.right;
   
   let svg = d3.select('body')
@@ -47,7 +43,7 @@ function d3Commands() {
     .attr('height', height + margin.top + margin.bottom)
     .attr('width', width + margin.left + margin.right)
     .append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.right + ')');
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
   const tooltip = d3.select('body')
     .append('div')
@@ -59,27 +55,26 @@ function d3Commands() {
     // turns lat and long into drawable paths
   let path = d3.geoPath();
 
-  
-  
   let counties = topojson.feature(datasetJSON, datasetJSON.objects.counties).features;
-  // console.log(datasetJSON);
-  
-  // console.log(counties);
   
   svg.append('g')
-  .attr('class', 'counties')
   .selectAll('path')
   .data(counties)
   .enter().append('path')
   .attr('class', 'county')
+  .attr("transform", `translate(0,${margin.top})`)
   .attr('data-fips', (d, i) => {
-    // console.log(educationDataset[i].fips);
+    // console.log(d);
     return educationDataset[i].fips;
   })
   .attr('data-education', (d, i) => {
-    // console.log(educationDataset[i].bachelorsOrHigher);
-    // console.log(educationDataset)
     return educationDataset[i].bachelorsOrHigher;
+  })
+  .attr('data-areaname', (d, i) => {
+    return educationDataset[i].area_name;
+  })
+  .attr('data-state', (d, i) => {
+    return educationDataset[i].state;
   })
   .attr('fill', (d, i) => determineColor(educationDataset[i].bachelorsOrHigher))
   .attr('d', path)
@@ -88,12 +83,23 @@ function d3Commands() {
   .on('mouseover', function(d, i) {
       tooltip.transition()
         .duration(0)
-        .attr('data-education', (d, i) => this.getAttribute('data-education'))
+        .attr('data-education', () => this.getAttribute('data-education'))
         .style('opacity', 0.9);
-      tooltip.html((d, i) => `${educationDataset[i].area_name}, ${educationDataset[i].state} ${this.getAttribute('data-education')}%`)
+      tooltip.html( function() {
+        // get result by matching fips with id
+          var result = educationDataset.filter(function( obj ) {
+            return obj.fips == d.id;
+          });
+          // if there is a result use it
+          if(result[0]){
+            return result[0]['area_name'] + ', ' + result[0]['state'] + ': ' + result[0].bachelorsOrHigher + '%'
+          }
+          //could not find a matching fips id in the data
+          return 0
+        })
         .style('left', (d3.event.pageX) + 10 + 'px')
         .style('top', (d3.event.pageY) + 'px')
-    })
+      })
     .on('mouseout', function(d, i) {
       tooltip.transition()
         .duration(200)
@@ -105,17 +111,65 @@ function d3Commands() {
   // get the data for states, access features
   let states = topojson.feature(datasetJSON, datasetJSON.objects.states).features;
   
-  // console.log(states);
-  
   svg.append('g')
-    .attr('class', 'state')
     .selectAll('path')
     .data(states)
     .enter().append('path')
     .attr('class', 'state')
+    .attr("transform", `translate(0,${margin.top})`)
     .attr('d', path)
     .attr('stroke-width', 0.5)
     .attr('stroke', 'white');
+
+    // LEGEND
+  let legend = svg.append("g")
+    .attr("class", "legend")
+    .attr('id', 'legend')
+    .attr("transform","translate(0,0)")
+    .style("font-size","12px")
+    .selectAll('g')
+    .data([
+       0,
+       13,
+       26,
+       39,
+       43,
+    ])
+    .enter().append('g');
+
+  legend.append('rect')
+    .attr('y', (d, i) => 70)
+    .attr('x', (d, i) => (width / 1.655) + (i * 60))
+    .attr('height', 10)
+    .attr('width', 60)
+    .attr('fill', (d, i) => determineColor(d))
+    .attr('stroke-width', 1.5)
+    .attr('stroke', 'black');
+
+  legend.append('text')
+    .attr('y', (d, i) => 93)
+    .attr('x', (d, i) => (width / 1.65) + (i * 60))
+    .style('fill', 'black')
+    .attr('weight', 'bold')
+    .attr('font-size', '10pt')
+    .text((d) => '' + (d) + '%');
+
+     // TITLE
+  svg.append('text')
+    .attr("y", (15))
+    .attr("x", (width / 2))
+    .style("text-anchor", "middle")
+    .attr('id', "title")
+    .attr('font-size', '28pt')
+    .attr('font-weight', 'bold')
+    .text("United States Educational Attainment");
+  // DESCRIPTION
+  svg.append('text')
+    .attr("y", (35))
+    .attr("x", (width / 2))
+    .style("text-anchor", "middle")
+    .attr('id', "description")
+    .text("Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)");
 }
 
 function determineColor(data) {
